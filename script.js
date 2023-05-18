249858c328fb082d181f2e2fa2c82e6d5c114e21
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-18 13:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-function letterCombinations(digits) {
-     
-    if(digits == ""){
-        return [];
-    }
-    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
-     
-    let res =[];
-    let que = [''];
-     
-    while(que.length>0){
-        let str = que[0];
-        que.shift();
-         
-        if(str.length == digits.length){
-            res.push(str); // if all digits are replaced with char push to result
-        } else{
-//             get the current number from the digits i.e if str.length = 2 , digits =123 s= 3
-            let s= Number(digits.charAt(str.length));
-            let val = table[s]; // get char from the table i.e def for s =3
-             
-            for(i=0;i<val.length;i++){
-                que.push(str+val.charAt(i));
-            }
-        }
-    }
-     
-    return res;
-     
+const digitToLetters = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+  '0': '0',
+  '1': '1'
+};
+
+function generateCombinations() {
+  var input = document.getElementById('input').value;
+  var combinations = [];
+
+  backtrack(combinations, '', input);
+
+  var resultElement = document.getElementById('result');
+  resultElement.innerHTML = '';
+
+  for (var i = 0; i < combinations.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = combinations[i];
+    resultElement.appendChild(li);
+  }
 }
 
-    let str = "23";
-    document.write(letterCombinations(str)); 
+function backtrack(combinations, current, digits) {
+  if (digits.length === 0) {
+    combinations.push(current);
+    return;
+  }
+
+  var letters = digitToLetters[digits[0]];
+
+  for (var i = 0; i < letters.length; i++) {
+    backtrack(combinations, current + letters[i], digits.slice(1));
+  }
 }
+
+var generateBtn = document.getElementById('generateBtn');
+generateBtn.addEventListener('click', generateCombinations);
