4aea691161358195cfe4199b8832eeda932059ac
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-31 17:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 const digitToLetters = {
-  '2': 'abc',
-  '3': 'def',
-  '4': 'ghi',
-  '5': 'jkl',
-  '6': 'mno',
-  '7': 'pqrs',
-  '8': 'tuv',
-  '9': 'wxyz',
-  '0': '0',
-  '1': '1'
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+  0: "0",
+  1: "1",
 };
 
-function generateCombinations() {
-  var input = document.getElementById('input').value;
-  var combinations = [];
+function generateCombinations(event) {
+  event.preventDefault();
 
-  backtrack(combinations, '', input);
+  const digits = document.getElementById("digits").value;
 
-  var resultElement = document.getElementById('result');
-  resultElement.innerHTML = '';
-
-  for (var i = 0; i < combinations.length; i++) {
-    var li = document.createElement('li');
-    li.textContent = combinations[i];
-    resultElement.appendChild(li);
-  }
+  const combinations = letterCombinations(digits);
+  document.getElementById("result").textContent =
+    "Combinations: " + combinations.join(", ");
 }
 
-function backtrack(combinations, current, digits) {
+function letterCombinations(digits) {
   if (digits.length === 0) {
-    combinations.push(current);
-    return;
+    return [];
   }
 
-  var letters = digitToLetters[digits[0]];
+  const combinations = [""];
 
-  for (var i = 0; i < letters.length; i++) {
-    backtrack(combinations, current + letters[i], digits.slice(1));
+  for (let i = 0; i < digits.length; i++) {
+    const letters = digitToLetters[digits[i]];
+
+    while (combinations[0].length === i) {
+      const currentCombination = combinations.shift();
+
+      for (let j = 0; j < letters.length; j++) {
+        combinations.push(currentCombination + letters[j]);
+      }
+    }
   }
+
+  return combinations;
 }
-
-var generateBtn = document.getElementById('generateBtn');
-generateBtn.addEventListener('click', generateCombinations);
